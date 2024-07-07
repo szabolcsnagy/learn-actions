26ec38edef498be04051cc53dec877ba5cb70fe2
Szabolcs Nagy <1876846+szabolcsnagy@users.noreply.github.com>
2024-07-07 03:27:37 -0400

    Using environment variables in tests

--- app/root.test.js
+++ app/root.test.js
@@ -1,5 +1,11 @@
 import {test, expect} from 'vitest'
+import {config} from 'dotenv/config';
 
 test('test', () => {
   expect(1).toBe(1);
 });
+
+test('db passwd defined in .env', () => {
+  // this should be set as repo secret in https://github.com/szabolcsnagy/learn-actions/settings/secrets/actions
+  expect(process.env.DB_PASSWD).toBeDefined();
+})
